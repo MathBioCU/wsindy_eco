9e4bd9d9e830359e5b389b21c75ad62b165db79f
Daniel Messenger <daniel.messenger@colorado.edu>
2024-05-19 20:31:47 -0600

    IC sampling

--- dukic_collab_fixedIC_sweep.m
+++ dukic_collab_fixedIC_sweep.m
@@ -1,12 +1,12 @@
 ntrain_inds = [-5];
 peak_width = 2;
-rngs = 1:100;
+rngs = 1:500;
 snr_Ys = 0.05;
 snr_X = 0.005;
 train_time_frac = 0.75;
 subsamp_t = 2;
 maxits_wendy = 5;
-use_true_IC = 'true';  'false';
+use_true_IC = 'true'; 
 
 addpath(genpath('wsindy_obj_base'))
 
@@ -29,7 +29,7 @@ WENDy_args = {'maxits_wendy',maxits_wendy,...
     'lambdas',10.^linspace(-4,0,50),'alpha',0.01,...
     'ittol',10^-4,'diag_reg',10^-6,'verbose',0};
 autowendy = 0.95; % increment library approximate confidence interval with this confidence level 
-X_var = 'True';
+toggle_X_var = 'true';
 tol = 5; % default heuristic increment, chosen when autowendy = 0.5;
 tol_min = 0.1; % lower bound on rel. residual to increment library, in case covariance severely underestimated
 tol_dd_learn = 10^-8;% ODE tolerance for forward solves in computing Y(T)
@@ -64,7 +64,7 @@ for kk=1:length(snr_Ys)
     libs_cell = cell(length(ntrain_inds),length(rngs));
     
     for ii=1:length(ntrain_inds)
-        parfor jj=1:length(rngs)
+        for jj=1:length(rngs)
             disp([subsamp_t kk ii jj])
             num_train_inds = ntrain_inds(ii);      
 
@@ -79,7 +79,11 @@ for kk=1:length(snr_Ys)
             [Y_train,X_train,train_inds,train_time,nstates_X,nstates_Y,X_in,sigma_X,sigma_Y,nX,nY] = ...
                 format_data(Ycell,X,t_epi,subsamp_t,train_time_frac,num_train_inds,...
                 test_length,snr_X,snr_Y,noise_alg_X,noise_alg_Y,gensamp_seed,noise_seed);
-    
+
+            if isequal(toggle_X_var,'true')
+                X_var = max(sigma_X,0);
+            end
+
             tic,
             %%% run alg
             [rhs_Y,W_Y,rhs_X,W_X,...
